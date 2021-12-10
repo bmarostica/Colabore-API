b64831014d890751dac17b20f0a5df6dc2f600eb
bianca.marostica <bmarostica@gmail.com>
2021-12-10 13:21:11 -0300

    criada classe categoria e realizado o mapeamento de banco de dados

---
+++ src/main/java/com/dbc/colabore/entity/CategoriaEntity.java
@@ -0,0 +1,24 @@
+package com.dbc.colabore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity(name = "CATEGORIA")
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CATEGORIA")
+    private String idCategoria;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_campanha", referencedColumnName = "id_campanha")
+    private CampanhaEntity campanha;
+}
