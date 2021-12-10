9c25d33437edb6cc55e30c1ec10fb79033934731
bianca.marostica <bmarostica@gmail.com>
2021-12-10 09:37:53 -0300

    adicionado anotação no controller de campanha

--- src/main/java/com/dbc/colabore/controller/CampanhaController.java
+++ src/main/java/com/dbc/colabore/controller/CampanhaController.java
@@ -5,6 +5,7 @@ import com.dbc.colabore.dto.CampanhaDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import javax.validation.Valid;
 @Validated
 public class CampanhaController {
 
+    @PostMapping
     public CampanhaDTO create(@RequestBody @Valid CampanhaCreateDTO campanhaCreateDTO){
         return null;
     }
