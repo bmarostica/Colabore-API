23e4df6d7edcb2c3eac15d002492df21e14ba5aa
bianca.marostica <bmarostica@gmail.com>
2021-12-10 13:50:57 -0300

    mapeamento camapnha entity

--- src/main/java/com/dbc/colabore/entity/CampanhaEntity.java
+++ src/main/java/com/dbc/colabore/entity/CampanhaEntity.java
@@ -3,24 +3,58 @@ package com.dbc.colabore.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
+@Entity(name = "CAMPANHA")
 public class CampanhaEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CAMPANHA")
     private Integer idCampanha;
+
+    @Column(name = "ID_USUARIO")
+    private UsuarioEntity idUsuario;
+
+    @Column(name = "FOTO")
     private String foto;
+
+    @Column(name = "TITULO_CAMPANHA")
     private String tituloCampanha;
+
+    @Column(name = "META_ARRECADACAO")
     private BigDecimal metaArrecadacao;
+
+    @Column(name = "TOTAL_ARECADADO")
     private BigDecimal totalArrecadado;
-    private List<String> tagsCategoria;
+
+    @OneToMany(mappedBy = "campanhaEntity", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CategoriaEntity> tagsCategoria;
+
+    @Column(name = "ULTIMA_ALTERACAO")
     private LocalDateTime ultimaAlteracao;
+
+
+    @Enumerated
+    @Column(name = "STATUS_CAMPANHA")
     private StatusCampanha statusCampanha;
+
+
+    @Column(name = "DESCRICAO_CAMPANHA")
     private String descricaoCampanha;
-    private UsuarioEntity criadorCampanha;
+
+    @ManyToMany
+    @JoinTable(
+            name = "USUARIO_CAMPANHA",
+            joinColumns = @JoinColumn(name = "id_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "id_campanha")
+    )
     private List<UsuarioEntity> usuariosContribuidores;
 
 
