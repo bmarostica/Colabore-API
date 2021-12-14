64180b631c1e155c808317c18c0fc7bb564f9bd3
tiagocoelhosilva86 <tiagospaik63@gmail.com>
2021-12-14 20:21:57 -0300

    fix heroku

--- src/main/java/com/dbc/colabore/config/FileStorageProperties.java
+++ src/main/java/com/dbc/colabore/config/FileStorageProperties.java
@@ -6,26 +6,26 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
 
-    @Value("${file.upload-dir-cammpanha}")
-    private String uploadDirCampanha;
-
-    @Value("${file.upload-dir-usuario}")
-    private String uploadDirUsuario;
-
-    public String getUploadDirCampanha() {
-        return uploadDirCampanha;
-    }
-
-    public void setUploadDirCampanha(String uploadDirCampanha) {
-        this.uploadDirCampanha = uploadDirCampanha;
-    }
-
-
-    public String getUploadDirUsuario() {
-        return uploadDirUsuario;
-    }
-
-    public void setUploadDirUsuario(String uploadDirUsuario) {
-        this.uploadDirUsuario = uploadDirUsuario;
-    }
+//    @Value("${file.upload-dir-cammpanha}")
+//    private String uploadDirCampanha;
+//
+//    @Value("${file.upload-dir-usuario}")
+//    private String uploadDirUsuario;
+//
+//    public String getUploadDirCampanha() {
+//        return uploadDirCampanha;
+//    }
+//
+//    public void setUploadDirCampanha(String uploadDirCampanha) {
+//        this.uploadDirCampanha = uploadDirCampanha;
+//    }
+//
+//
+//    public String getUploadDirUsuario() {
+//        return uploadDirUsuario;
+//    }
+//
+//    public void setUploadDirUsuario(String uploadDirUsuario) {
+//        this.uploadDirUsuario = uploadDirUsuario;
+//    }
 }
